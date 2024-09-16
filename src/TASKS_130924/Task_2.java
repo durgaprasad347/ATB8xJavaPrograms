8e3a23bf771a794c33b55a10ecd8434e4d54b0b5
Durga prasad <durgaprasad409633@gmail.com>
2024-09-15 22:19:03 -0700

    Add the files

---
+++ src/TASKS_130924/Task_2.java
@@ -0,0 +1,15 @@
+package TASKS_130924;
+
+public class Task_2 {
+    public static void main(String args[]){
+        double a = 34;
+        double b = 10;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
